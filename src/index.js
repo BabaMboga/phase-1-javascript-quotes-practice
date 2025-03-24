19d4161eab4ea4dcd53813ab6b96d98a713ca3a4
Ayim William <odhisayim@gmail.com>
2025-03-24 16:27:28 +0300

    incomplete renderQuote function

--- src/index.js
+++ src/index.js
@@ -11,5 +11,18 @@ document.addEventListener('DOMContentLoaded', () => { //waits for DOM to load so
             } );
     }
 
-    
+    function renderQuote(quote){
+        const li = document.createElement('li');
+        li.classList.add('quote-card');
+
+        li.innerHTML = `
+            <blockquote class="blockquote">
+                <p class="mb-0">${quote.quote}</p>
+                <footer class="blockquote-footer">${quote.author}</footer>
+                <br>
+                <button class='btn-success' data-id="${quote.id}">Likes: <span>${}</span></button>
+                <button class='btn-danger'>Delete</button>
+            </blockquote>
+        `
+    }
 })
